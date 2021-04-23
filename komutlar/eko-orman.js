935f9d77b659284684aa7742b4e63fa4a64105d3
erentrkk0 <68477904+erentrkk0@users.noreply.github.com>
2021-04-23 03:03:02 +0300

    Update eko-orman.js

--- komutlar/eko-orman.js
+++ komutlar/eko-orman.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');//Fiber <3 LysteX
+const Discord = require('discord.js');
 const db = require('quick.db')
   const ms = require('parse-ms');
 exports.run = async(client, message, args) => {
